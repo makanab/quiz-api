e16a98e61884be7db072a552acf1b9ed42956e0a
makanab <makanabrian1@gmail.com>
2020-11-25 13:00:42 +0300

    quiz update

--- routes/index.route.js
+++ routes/index.route.js
@@ -1,9 +1,11 @@
 const route = require('express').Router();
 const fs = require('fs');
+const db = fs.readFileSync('./db.json')
 
 route.get('/quiz',async (req,res,next)=>{
-    const db = await fs.readFileSync('./db.json')
-    res.send(JSON.parse(db))
+    
+    let data = await JSON.parse(db)
+    res.status(200).json(data);
        
    })
 
