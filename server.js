bf8099bf38fdf0326c25f68bced1c307f503cda3
makanab <makanabrian1@gmail.com>
2020-11-26 09:06:49 +0300

    cors

--- server.js
+++ server.js
@@ -2,12 +2,13 @@
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 const express = require('express');
+const  cors = require('cors');
 const app = express();
 
 
 const rts = require('./routes/index.route');
-
-app.use('/api',rts)
+app.use('/api',rts);
+app.use(cors());
 
 
 
